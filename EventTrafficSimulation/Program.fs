794fa1abcf6131d50f30f277ac5fd2866bb6b423
TWolverson <t_wolverson@hotmail.com>
2015-04-26 18:19:39 +0100

    Add the RR67 satflow model (wip)

--- EventTrafficSimulation/Program.fs
+++ EventTrafficSimulation/Program.fs
@@ -2,76 +2,6 @@
 // See the 'F# Tutorial' project for more help.
 open System
 
-type PCU = 
-    | Traffic of float
-    | Zero
-
-type Tick =
-    | Timestep of int
-
-type packet = {
-    Flow : PCU;
-    Origin : Guid;
-    ArriveAt : Tick;
-    }
-
-type Msg = 
-    | Tick of Tick
-    | Packet of packet
-
-type Link = {
-    Length : int;
-}
-
-type Junction = {
-    CycleTime : int;
-}
-
-let BuildLinks() = 
-    [for i in 1 .. 10 -> { Length = 200 }]
-
-let BuildJunctions() =
-    [for i in 1 .. 10 -> { CycleTime = 120 }]
-
-let BuildNetwork() =
-    (BuildLinks() , BuildJunctions())
-    
-let simulation = MailboxProcessor.Start(fun agent ->
-    let rec loop network count = async {
-        let! msg = agent.Receive()
-        match msg with
-        | Tick t ->
-            // todo: release all held until this tick
-            match t with
-            | Timestep t' -> printfn "%i" t'
-        | Packet p ->
-            let receivers = [1,2,3]
-            for i in receivers do agent.Post(Packet {
-                Flow = match p.Flow with | Traffic f -> Traffic (f/3.0) | Zero -> Zero; 
-                Origin = Guid.NewGuid(); 
-                ArriveAt = Timestep 5
-                })
-        }
-    loop (BuildNetwork()) 0
-)
-//    let simulation = MailboxProcessor.Start(fun agent ->
-//        let rec loop network count = async {
-//            let! msg = agent.Receive()
-//            match msg with
-//            | Tick t ->
-//                // todo: release all held until this tick
-//                match t with
-//                | Timestep t' -> printfn "%i" t'
-//            | Packet p ->
-//                let receivers = [1,2,3]
-//                for i in receivers do agent.Post(Packet {
-//                    Flow = match p.Flow with | Traffic f -> Traffic (f/3.0) | Zero -> Zero; 
-//                    Origin = Guid.NewGuid(); 
-//                    ArriveAt = Timestep 5
-//                    })
-//            }
-//        loop (BuildNetwork()) 0
-//    )
 type msg = 
         | CreateAgent
         | DoStuff of string
@@ -79,7 +9,6 @@ type msg =
 
 [<EntryPoint>]
 let main argv = 
-
     let pool = MailboxProcessor.Start(fun master ->
         let rec messageloop agents = async {
             let! msg = master.Receive()
@@ -88,6 +17,13 @@ let main argv =
                 printfn "Received CreateAgent"
                 let newagent = MailboxProcessor.Start(fun agent ->
                     let rec forever = async {
+                        let! ggg = agent.Scan(fun m -> 
+                            let condition t = 
+                                t = "hello"
+                            match m with
+                            | DoStuff t when condition t -> 
+                                Some( async { () }) 
+                            | _ -> None);
                         let! mymsg = agent.Receive();
                         match mymsg with
                         | DoStuff t ->
---
+++ EventTrafficSimulation/RR67.fs
@@ -0,0 +1,59 @@
+﻿module RR67
+
+[<Measure>] type pcu
+[<Measure>] type h
+[<Measure>] type flow = pcu/h
+[<Measure>] type flowratio = flow^-1 // not sure if this is needed/right
+[<Measure>] type m
+
+let f = 4.0 // proportion of right-turners
+let S0 = 1234.0<flow>
+let r = 4.0<m> // turn radius
+let q0 = 4.5<flow>
+let lm = 0.5 // proportion of cycle which is effective green; <.0
+let nl = 2 //number of opposing lanes
+let so = 455.<flow> //saturation flow per lane of opposing entry
+let X0 = q0 / (lm * float nl * so)
+let Ns = 1234.6<pcu>
+let t1 = 12.0 * (X0 ** 2.0) / (1.0 + 0.6 * (1.0 - f) * Ns)
+let t2 = 1.0 - (f * (X0 ** 2.0))
+let T =  1.0 + 1.5<m> / r + t1 / t2
+
+
+
+let Sg = (S0 - 230.0<flow>) / (1.0 + (T - 1.0) * f)
+
+type EntryGeometry = {
+    NumLanes : int;
+    SatFlowPerLane : float<flow>;
+    StorageSpace : float<pcu> option
+}
+
+type JunctionGeometry = {
+    Measured : EntryGeometry;
+    Opposing : EntryGeometry option;
+}    
+
+type Stage = {
+    StartTime : int;
+    EndTime : int;
+}
+
+type SignalSettings = {
+    Stages : Stage list
+}
+
+type Model = {
+    Geometry : JunctionGeometry;
+    Signals : SignalSettings option;
+}    
+
+let OpposedCase f lm nl so = 
+    let X0 = q0 / (lm * float nl * so)
+    let Ns = 1234.6
+    let t1 = 12.0 * (X0 ** 2.0) / (1.0 + 0.6 * (1.0 - f) * Ns)
+    let t2 = 1.0 - (f * (X0 ** 2.0))
+    let T =  1.0 + 1.5 / r + t1 / t2
+    (S0 - 230.0) / (1.0 + (T - 1.0) * f)
+
+let DefaultOpposing f lm = OpposedCase f lm 1 1800.0
